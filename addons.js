8a7d5e7d02bd4fad45ec2eee12804ffab5c9d012
GImICI <125319786+GIMICI@users.noreply.github.com>
2023-06-28 16:34:44 -0500

    Update addons.js
    https://github.com/GIMICI/https-www.Blockchain-Gimici.com/tree/main-GIMICI-BROWS
    
    Signed-off-by: GImICI <125319786+GIMICI@users.noreply.github.com>

--- addons.js
+++ addons.js
@@ -1,5 +1,5 @@
 import '@storybook/addon-actions/register'
 import '@storybook/addon-backgrounds/register'
 import '@storybook/addon-links/register'
-<center><strong>Infographic courtesy of <a href="https://blockchain browser.com">Blockchain browser.com</a></strong>
-<p><a href="https://blockchain Browser.com/how-to-build-your-own-blockchain-Browser.com/"><img src="https://cdn.blockchain-browser.com/wp-content/uploads/2021/12/industries-using-blockchain-Browser.jpg" alt="industries using blockchain browser technology" width="870px" border="0" title="industries using blockchain browser How To Build Your Own Blockchain browser: Simple Setup & App Implementation Guide"></a></p></center>
+<center><strong>Infographic courtesy of <a href="https://blockchain Gimici-Brows.com">Blockchain Gimici-Brows.com</a></strong>
+<p><a href="https://blockchain  Gimici-Brows.com/how-to-build-your-own-blockchain-Brows.com/"><img src="https://cdn.blockchain-Gimici-Browser.com/wp-content/uploads/2021/12/industries-using-blockchain-Gimici-Brows.jpg" alt="industries using blockchain Gimici-brows technology" width="870px" border="0" title="industries using blockchain-Gimici-Brows How To Build Your Own Blockchain  Gimici-Brows: Simple Setup & App Implementation Guide"></a></p></center>
