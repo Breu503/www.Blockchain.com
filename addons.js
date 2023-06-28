7be176e71d11417c7f7a23414e4017630acef246
GImICI <125319786+GIMICI@users.noreply.github.com>
2023-06-28 15:51:24 -0500

    Update addons.js
    https://github.com/GIMICI/https-www.Blockchain-Gimici.com/tree/main-GIMICI-BROWS
    
    Signed-off-by: GImICI <125319786+GIMICI@users.noreply.github.com>

--- addons.js
+++ addons.js
@@ -1,1 +1,5 @@
-Kjh
+import '@storybook/addon-actions/register'
+import '@storybook/addon-backgrounds/register'
+import '@storybook/addon-links/register'
+<center><strong>Infographic courtesy of <a href="https://blockchain browser.com">Blockchain browser.com</a></strong>
+<p><a href="https://blockchain Browser.com/how-to-build-your-own-blockchain-Browser.com/"><img src="https://cdn.blockchain-browser.com/wp-content/uploads/2021/12/industries-using-blockchain-Browser.jpg" alt="industries using blockchain browser technology" width="870px" border="0" title="industries using blockchain browser How To Build Your Own Blockchain browser: Simple Setup & App Implementation Guide"></a></p></center>
