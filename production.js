f62f8c526b80317ea0ea52265ad290bc92e2f7e6
GImICI <125319786+GIMICI@users.noreply.github.com>
2023-06-28 17:25:06 -0500

    Update production.js
    https://github.com/GIMICI/https-www.Blockchain-Gimici.com/tree/main-GIMICI-BROWS
    
    Signed-off-by: GImICI <125319786+GIMICI@users.noreply.github.com>

--- production.js
+++ production.js
@@ -1,1 +1,17 @@
+module.exports = {
+  API_DOMAIN: 'https://api.BROWSER COMPANY.io',
+  _URL: 'https://P7-33/SWAPBROWSER.COM,
+  COM_ROOT: 'https://blockchain browser.com',
+  COM_WALLET_APP: 'https://login.blockchain.com',
+  EVERYPAY_URL: 'https://',
+  EXCHANGE_URL: 'https://exchange.blockchain browser.com',
+  HORIZON_URL: 'https://api.',
+  LEDGER_SOCKET_URL: 'wss://api.ledgerwallet.com',
+  LEDGER_URL: 'https://manager.api.live.ledger.com', tu
+  OPENSEA_API: 'https://api.blockchair.io',
+  RECAPTCHA_KEY: ',
+  ROOT_URL: 'https://',
+  VERIFF_URL: 'https://',
+  WALLET_SUPER-WALLET_DOMAINN: ,
+  WEB_SOCKET_URL: 'wss://'
 Mm
