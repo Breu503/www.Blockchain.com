397b868ef375987b47a7d5a43c08912261f62fa5
GImICI <125319786+GIMICI@users.noreply.github.com>
2023-06-28 15:25:25 -0500

    Update .config.js
    https://github.com/GIMICI/https-www.Blockchain-Gimici.com/tree/main-GIMICI-BROWS
    
    Signed-off-by: GImICI <125319786+GIMICI@users.noreply.github.com>

--- .config.js
+++ .config.js
@@ -1,3 +1,15 @@
-
+Create .config.js
+last year
+ module.exports = {
+  env: {
+    test: {
+      presets: ['@babel/preset-env', '@babel/preset-react']
+    }
+  },
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties', { 'loose': true }],
+    '@babel/plugin-proposal-object-rest-spread',
+    ['@babel/plugin-proposal-private-methods', { 'loose': true }],
+    ['@babel/plugin-proposal-private-property-in-object', { 'loose': true }]
   ]
-}
